514d82eccb8be4dc193e7eb3f5d6dc00d235f0bb
Keshawn Strong <strongkeshawn047@gmail.com>
2023-03-24 22:35:30 -0700

    #4: Allow users to send notifications on when they want to be notified about a task

---
+++ task360/taskapp/tasks.py
@@ -0,0 +1,21 @@
+import time
+
+from celery import shared_task
+from django.core.mail import send_mail
+
+
+@shared_task()
+def periodic_email(topic:str, msg:str, _from:str, to:str) -> None:
+
+    send_mail(
+        subject=topic,
+        message=msg, 
+        from_email=_from, 
+        recipient_list=[to]
+    )
+
+    time.sleep(1)
+    return "Email sent"
+
+
+
